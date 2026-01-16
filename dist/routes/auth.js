6285af1837407f617602f5a0cc0967a9fb8dbfe1
ehtan <ehtan@kali.ehtan>
2026-01-16 13:15:53 -0500

    Hardcode Discord OAuth URL

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -214,10 +214,7 @@ router.post('/discord/callback', async (req, res) => {
 });
 // Get Discord OAuth URL
 router.get('/discord/url', (req, res) => {
-    const clientID = config.DISCORD_CLIENT_ID;
-    const redirectURL = encodeURIComponent(config.DISCORD_CALLBACK_URL);
-    const scope = encodeURIComponent('identify email');
-    const url = `https://discord.com/oauth2/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURL}&scope=${scope}`;
+    const url = 'https://discord.com/oauth2/authorize?client_id=1436396594214867115&response_type=code&redirect_uri=https%3A%2F%2Fsafhgt.onrender.com%2Fapi%2Fauth%2Fdiscord%2Fcallback&scope=identify';
     res.json({ url });
 });
 // Get current user
