b045e64748c3f23ccf97cbb44d7de8364699842d
ehtan <ehtan@kali.ehtan>
2026-01-17 03:02:32 -0500

    dfdf

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,14 +2,29 @@ import mongoose from 'mongoose';
 
 export enum UserRole {
   DEFAULT = 'default',
+  BETA_TESTER = 'beta_tester',
   JUNIOR_HELPER = 'junior_helper',
   HELPER = 'helper',
   MODERATOR = 'moderator',
   SR_MODERATOR = 'sr_moderator',
+  DEPUTY = 'deputy',
   ADMIN = 'admin',
   MAIN_ADMIN = 'main_admin',
   OWNER = 'owner'
 }
+
+export const roleColors: Record<UserRole, string> = {
+  [UserRole.DEFAULT]: '#808080',
+  [UserRole.BETA_TESTER]: '#FF1493',
+  [UserRole.JUNIOR_HELPER]: '#0000FF',
+  [UserRole.HELPER]: '#87CEEB',
+  [UserRole.MODERATOR]: '#00AA00',
+  [UserRole.SR_MODERATOR]: '#FFFF00',
+  [UserRole.DEPUTY]: '#FF0000',
+  [UserRole.ADMIN]: '#FF0000',
+  [UserRole.MAIN_ADMIN]: '#FF0000',
+  [UserRole.OWNER]: '#FF0000'
+};
 
 export interface IUser extends mongoose.Document {
   discordId: string;
